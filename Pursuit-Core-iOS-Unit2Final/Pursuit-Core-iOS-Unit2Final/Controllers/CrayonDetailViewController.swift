481919107444549af206d4221759d74b6f925b76
janezhu1618 <janezhu@pursuit.org>
2018-11-30 14:50:41 -0500

    slider labels adjust according to slider values

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/CrayonDetailViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/CrayonDetailViewController.swift
@@ -21,28 +21,41 @@ class CrayonDetailViewController: UIViewController {
     @IBOutlet weak var blueLabel: UILabel!
     @IBOutlet weak var alphaStepper: UIStepper!
     @IBOutlet weak var alphaLabel: UILabel!
-    var currentRedSliderValue: Float = 0.0 {
-        didSet {
-            redLabel.text = "Red: " + String(format: "%.2f", currentRedSliderValue)
-    }
-    }
-
-    var currentGreenSliderValue: Float = 0.0 {
-        didSet {
-            greenLabel.text = "Green: " + String(format: "%.2f", currentGreenSliderValue)
+    var currentRedSliderValue: CGFloat {
+        get {
+            return CGFloat(crayon.red/255)
+        }
+        set {
+            redLabel.text = "Red: " + String(format: "%.2f", newValue)
         }
     }
-    var currentBlueSliderValue: Float = 0.0 {
-        didSet {
-            blueLabel.text = "Blue: " + String(format: "%.2f", currentBlueSliderValue)
+    var currentGreenSliderValue: CGFloat {
+        get {
+            return CGFloat(crayon.green/255)
+        }
+        set {
+            greenLabel.text = "Green: " + String(format: "%.2f", newValue)
         }
     }
-    
-
+    var currentBlueSliderValue: CGFloat {
+        get {
+            return CGFloat(crayon.blue/255)
+        }
+        set {
+            blueLabel.text = "Blue: " + String(format: "%.2f", newValue)
+        }
+    }
+    var currentAlphaStepperValue: CGFloat {
+        get {
+            return 1
+        }
+        set {
+            alphaLabel.text = "Alpha: " + String(format: "%.1f", currentAlphaStepperValue)
+        }
+    }
     fileprivate func sliderAndLabelSetUp(color: String, colorDouble: Double, slider: UISlider, label: UILabel) {
         label.text = "\(color): " + (String(format: "%.2f", colorDouble/255))
         slider.value = Float(colorDouble/255)
-        print(slider.value)
         slider.maximumValue = 1
         slider.minimumValue = 0
     }
@@ -54,26 +67,43 @@ class CrayonDetailViewController: UIViewController {
         sliderAndLabelSetUp(color: "Red", colorDouble: crayon!.red, slider: redSlider, label: redLabel)
         sliderAndLabelSetUp(color: "Green", colorDouble: crayon!.green, slider: greenSlider, label: greenLabel)
         sliderAndLabelSetUp(color: "Blue", colorDouble: crayon!.blue, slider: blueSlider, label: blueLabel)
-        alphaLabel.text = "Alpha: 0"
-        
+        alphaLabel.text = "Alpha: 1"
+        alphaStepper.value = 1.0
+        alphaStepper.maximumValue = 1.0
+        alphaStepper.minimumValue = 0.0
+        alphaStepper.stepValue = 0.1
+    }
+    
+    private func updateBackground() {
+        view.backgroundColor = UIColor.init(red: currentRedSliderValue, green: currentGreenSliderValue, blue: currentBlueSliderValue, alpha: currentAlphaStepperValue)
     }
     
     @IBAction func redSlider(_ sender: UISlider) {
-        currentRedSliderValue = sender.value
+        currentRedSliderValue = CGFloat(sender.value)
+        updateBackground()
     }
     
     
     @IBAction func greenSlider(_ sender: UISlider) {
-        currentGreenSliderValue = sender.value
-        greenLabel.text = "Green: " + (String(format: "%.2f", sender.value/255))
+        currentGreenSliderValue = CGFloat(sender.value)
+        updateBackground()
     }
     
     @IBAction func blueSlider(_ sender: UISlider) {
-        currentBlueSliderValue = sender.value
-        blueLabel.text = "Blue: " + (String(format: "%.2f", sender.value/255))
+        currentBlueSliderValue = CGFloat(sender.value)
+        updateBackground()
     }
     
+    @IBAction func alphaStepper(_ sender: UIStepper) {
+        currentAlphaStepperValue = CGFloat(sender.value)
+        updateBackground()
+    }
     @IBAction func reset(_ sender: UIButton) {
         view.backgroundColor = UIColor.init(red: CGFloat(crayon.red/255), green: CGFloat(crayon.green/255), blue: CGFloat(crayon.blue/255), alpha: 1)
+        sliderAndLabelSetUp(color: "Red", colorDouble: crayon!.red, slider: redSlider, label: redLabel)
+        sliderAndLabelSetUp(color: "Green", colorDouble: crayon!.green, slider: greenSlider, label: greenLabel)
+        sliderAndLabelSetUp(color: "Blue", colorDouble: crayon!.blue, slider: blueSlider, label: blueLabel)
+        alphaLabel.text = "Alpha: 1"
+        alphaStepper.value = 1.0
     }
 }
