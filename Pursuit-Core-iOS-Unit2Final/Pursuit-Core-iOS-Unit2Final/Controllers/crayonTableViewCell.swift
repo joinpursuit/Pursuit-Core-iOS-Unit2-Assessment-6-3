1c47c5034af4b21bbe363665db398a61f6485577
Donkemezuo <raymonddonkemezuo.org>
2018-11-30 17:49:52 -0500

    Unit two final

---
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/crayonTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  crayonTableViewCell.swift
+//  Pursuit-Core-iOS-Unit2Final
+//
+//  Created by Donkemezuo Raymond Tariladou on 11/30/18.
+//  Copyright © 2018 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class crayonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Views/DetailedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailedViewController.swift
+//  Pursuit-Core-iOS-Unit2Final
+//
+//  Created by Donkemezuo Raymond Tariladou on 11/30/18.
+//  Copyright © 2018 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class DetailedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
