2ce5fbc55add3f1165aaff2160d40a2d65d544ab
MattHuie <matthuie93@gmail.com>
2018-11-30 14:14:26 -0500

    mvp done

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/DetailViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/DetailViewController.swift
@@ -20,22 +20,46 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var blueValueLabel: UILabel!
     @IBOutlet weak var alphaValueLabel: UILabel!
     
-    var crayons: Crayon?
+    var crayons: Crayon!
+    
  
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor(displayP3Red: CGFloat(crayons.red/255), green: CGFloat(crayons.green/255), blue: CGFloat(crayons.blue/255), alpha: CGFloat(alphaStepper!.value))
         updateUI()
+       
 
     }
     
     private func updateUI() {
-        colorNameLabel.text = crayons?.name
-       
-        
+        colorNameLabel.text = crayons.name
+        redSlider.setValue(Float(crayons.red), animated: true)
+        greenSlider.setValue(Float(crayons.green), animated: true)
+        blueSlider.setValue(Float(crayons.blue), animated: true)
+        alphaStepper.value = 1
+        redValueLabel.text = "Red: \(redSlider.value)"
+        greenValueLabel.text = "Green: \(greenSlider.value)"
+        blueValueLabel.text = "Blue: \(blueSlider.value)"
+        alphaValueLabel.text = "Alpha: \(alphaStepper.value)"
+    }
+    
+    
+    
+    
+    @IBAction func sliderChanged (sender: AnyObject) {
+        redValueLabel.text = "Red: \(redSlider.value)"
+        greenValueLabel.text = "Green: \(greenSlider.value)"
+        blueValueLabel.text = "Blue: \(blueSlider.value)"
+        alphaValueLabel.text = "Alpha: \(alphaStepper.value)"
+        self.view.backgroundColor = UIColor(displayP3Red: CGFloat(redSlider!.value)/255, green: CGFloat(greenSlider!.value)/255, blue: CGFloat(blueSlider!.value)/255, alpha: CGFloat(alphaStepper!.value))
         
         
     }
     
+    @IBAction func resetValues (sender: UIButton) {
+        updateUI()
+        self.view.backgroundColor = UIColor(displayP3Red: CGFloat(crayons.red/255), green: CGFloat(crayons.green/255), blue: CGFloat(crayons.blue/255), alpha: CGFloat(alphaStepper!.value))
+    }
 
    
 }
