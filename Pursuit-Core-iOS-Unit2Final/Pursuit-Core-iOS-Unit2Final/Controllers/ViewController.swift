9fe37e95c2804e0deda79092fce2c9e08e6736dd
elizabethperaza@gmail.com <elizabethperaza@gmail.com>
2018-11-30 10:43:36 -0500

    working basic table that includes title and subtitle

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
@@ -10,14 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  var crayons = Crayon.allTheCrayons
+  
+  @IBOutlet weak var myTableView: UITableView!
+  
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-
-print("TEST TEST TEST")
-
+    myTableView.dataSource = self
 
   }
 
 
+}
+
+extension ViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return crayons.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = myTableView.dequeueReusableCell(withIdentifier: "crayonCell", for: indexPath)
+    
+    let currentColor = crayons[indexPath.row]
+    
+    cell.textLabel?.text = currentColor.name
+    cell.detailTextLabel?.text = currentColor.hex
+    
+    return cell
+    
+  }
 }
 
