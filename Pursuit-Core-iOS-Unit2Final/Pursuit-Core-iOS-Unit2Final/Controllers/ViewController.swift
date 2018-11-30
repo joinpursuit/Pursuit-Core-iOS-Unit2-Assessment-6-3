3075af4aec408b53e9120d89487dc61af6761bb1
MattHuie <matthuie93@gmail.com>
2018-11-30 11:33:56 -0500

    got segue and basic detailVC to work

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
@@ -18,6 +18,13 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     myTableView.dataSource = self
   }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let indexPath = myTableView.indexPathForSelectedRow,
+            let detailViewController = segue.destination as? DetailViewController else { return }
+        let crayon = Crayon.allTheCrayons[indexPath.row]
+        detailViewController.crayons = crayon
+    }
 
 
 }
@@ -29,10 +36,16 @@ extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = myTableView.dequeueReusableCell(withIdentifier: "colorsCell", for: indexPath)
+        let red = crayons[indexPath.row].red / 255
+        let green = crayons[indexPath.row].green / 255
+        let blue = crayons[indexPath.row].blue / 255
         cell.textLabel?.text = crayons[indexPath.row].name
         cell.detailTextLabel?.text = crayons[indexPath.row].hex
+        let backgroundColor = UIColor(displayP3Red: CGFloat(red), green: CGFloat(green), blue: CGFloat(blue), alpha: 1.0)
+        cell.contentView.backgroundColor = backgroundColor
         return cell
     }
     
     
 }
+
