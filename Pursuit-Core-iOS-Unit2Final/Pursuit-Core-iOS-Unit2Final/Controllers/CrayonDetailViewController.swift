0dbe05750036c5a55edfc8d4560b821564fa9232
janezhu1618 <janezhu@pursuit.org>
2018-11-30 12:56:27 -0500

    1st commit - set up orignal VC and detailVC

---
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/CrayonDetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CrayonDetailViewController.swift
+//  Pursuit-Core-iOS-Unit2Final
+//
+//  Created by Jane Zhu on 11/30/18.
+//  Copyright © 2018 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class CrayonDetailViewController: UIViewController {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    @IBOutlet weak var alphaStepper: UIStepper!
+    @IBOutlet weak var alphaLabel: UILabel!
+    @IBOutlet weak var resetButton: UIButton!
+    
+    var crayon: Crayon!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    nameLabel.text = crayon.name
+    }
+    
+
+}
--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var crayons: [Crayon] = Crayon.allTheCrayons
+    
+    @IBOutlet weak var tableView: UITableView!
 
   override func viewDidLoad() {
+    title = "Crayons"
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    tableView.dataSource = self
   }
 
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return crayons.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let crayon = crayons[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "crayonCell", for: indexPath)
+        cell.textLabel?.textColor = UIColor.white
+        cell.detailTextLabel?.textColor = UIColor.white
+        cell.textLabel?.text = crayon.name
+        cell.detailTextLabel?.text = crayon.hex
+        cell.backgroundColor = UIColor.init(red: CGFloat(crayon.red/255), green: CGFloat(crayon.green/255), blue: CGFloat(crayon.blue/255), alpha: 1)
+        return cell
+    }
+}
