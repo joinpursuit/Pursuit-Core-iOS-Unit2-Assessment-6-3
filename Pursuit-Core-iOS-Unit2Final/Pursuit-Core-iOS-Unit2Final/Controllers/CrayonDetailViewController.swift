ce7d90a72b8538b600b63e017d6b730e023efb63
janezhu1618 <janezhu@pursuit.org>
2018-11-30 15:02:04 -0500

    6th commit - slider colors adjust accordingly

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/CrayonDetailViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/CrayonDetailViewController.swift
@@ -21,40 +21,13 @@ class CrayonDetailViewController: UIViewController {
     @IBOutlet weak var blueLabel: UILabel!
     @IBOutlet weak var alphaStepper: UIStepper!
     @IBOutlet weak var alphaLabel: UILabel!
-    var currentRedSliderValue: CGFloat {
-        get {
-            return CGFloat(crayon.red/255)
-        }
-        set {
-            redLabel.text = "Red: " + String(format: "%.2f", newValue)
-        }
-    }
-    var currentGreenSliderValue: CGFloat {
-        get {
-            return CGFloat(crayon.green/255)
-        }
-        set {
-            greenLabel.text = "Green: " + String(format: "%.2f", newValue)
-        }
-    }
-    var currentBlueSliderValue: CGFloat {
-        get {
-            return CGFloat(crayon.blue/255)
-        }
-        set {
-            blueLabel.text = "Blue: " + String(format: "%.2f", newValue)
-        }
-    }
-    var currentAlphaStepperValue: CGFloat {
-        get {
-            return 1
-        }
-        set {
-            alphaLabel.text = "Alpha: " + String(format: "%.1f", currentAlphaStepperValue)
-        }
-    }
+    lazy var currentRedSliderValue = CGFloat(crayon.red/255)
+    lazy var currentGreenSliderValue = CGFloat(crayon.green/255)
+    lazy var currentBlueSliderValue = CGFloat(crayon.blue/255)
+    var currentAlphaStepperValue: CGFloat = 1
+    
     fileprivate func sliderAndLabelSetUp(color: String, colorDouble: Double, slider: UISlider, label: UILabel) {
-        label.text = "\(color): " + (String(format: "%.2f", colorDouble/255))
+        label.text = "\(color): " + String(format: "%.2f", colorDouble/255)
         slider.value = Float(colorDouble/255)
         slider.maximumValue = 1
         slider.minimumValue = 0
@@ -67,6 +40,7 @@ class CrayonDetailViewController: UIViewController {
         sliderAndLabelSetUp(color: "Red", colorDouble: crayon!.red, slider: redSlider, label: redLabel)
         sliderAndLabelSetUp(color: "Green", colorDouble: crayon!.green, slider: greenSlider, label: greenLabel)
         sliderAndLabelSetUp(color: "Blue", colorDouble: crayon!.blue, slider: blueSlider, label: blueLabel)
+        
         alphaLabel.text = "Alpha: 1"
         alphaStepper.value = 1.0
         alphaStepper.maximumValue = 1.0
@@ -75,12 +49,17 @@ class CrayonDetailViewController: UIViewController {
     }
     
     private func updateBackground() {
-        view.backgroundColor = UIColor.init(red: currentRedSliderValue, green: currentGreenSliderValue, blue: currentBlueSliderValue, alpha: currentAlphaStepperValue)
+        print(currentRedSliderValue)
+        print(currentGreenSliderValue)
+        print(currentBlueSliderValue)
+        print(currentAlphaStepperValue)
+        view.backgroundColor = UIColor.init(red: currentRedSliderValue, green: currentGreenSliderValue, blue: currentBlueSliderValue, alpha: CGFloat(currentAlphaStepperValue))
     }
     
     @IBAction func redSlider(_ sender: UISlider) {
         currentRedSliderValue = CGFloat(sender.value)
         updateBackground()
+        redLabel.text = "Red: " + String(format: "%.2f", currentRedSliderValue)
     }
     
     
@@ -97,6 +76,7 @@ class CrayonDetailViewController: UIViewController {
     @IBAction func alphaStepper(_ sender: UIStepper) {
         currentAlphaStepperValue = CGFloat(sender.value)
         updateBackground()
+        alphaLabel.text = "Alpha: " + String(format: "%.1f", currentAlphaStepperValue)
     }
     @IBAction func reset(_ sender: UIButton) {
         view.backgroundColor = UIColor.init(red: CGFloat(crayon.red/255), green: CGFloat(crayon.green/255), blue: CGFloat(crayon.blue/255), alpha: 1)
