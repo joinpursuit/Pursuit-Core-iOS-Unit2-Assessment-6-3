c7a92c162cf0829d805b0c1b08f3ba9eea7c2789
tawnyblvd <arielrivera@pursuit.org>
2018-11-30 10:27:25 -0500

    start of assessment

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
@@ -6,13 +6,14 @@
 //  Copyright © 2018 Alex Paul. All rights reserved.
 //
 
+
 import UIKit
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+   
   }
 
 
