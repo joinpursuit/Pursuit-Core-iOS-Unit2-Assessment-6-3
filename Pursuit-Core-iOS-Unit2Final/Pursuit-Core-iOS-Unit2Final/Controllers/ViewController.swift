8688b14ba774ddf3306ebd3c0b0bdcd79f626e70
Donkemezuo <raymonddonkemezuo.org>
2018-11-30 17:50:36 -0500

    Unit two final

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
@@ -9,12 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var allCrayonColors = Crayon.allTheCrayons
 
-  override func viewDidLoad() {
+    @IBOutlet weak var myCrayonTableView: UITableView!
+    override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-
+        
+        myCrayonTableView.dataSource = self
+        myCrayonTableView.delegate = self
+    
 }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+       guard let selectedRow = myCrayonTableView.indexPathForSelectedRow, let detailedViewInformations = segue.destination as? DetailedViewController else {return}
+        let choiceColor = allCrayonColors[selectedRow.row]
+        detailedViewInformations.color = choiceColor
+        let backgroundColorToDisplay = UIColor.init(displayP3Red: CGFloat(choiceColor.red)/255, green: CGFloat(choiceColor.green)/255, blue: CGFloat(choiceColor.blue)/255, alpha: 1.0)
+       detailedViewInformations.view.backgroundColor = backgroundColorToDisplay
+    
+    }
+}
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return allCrayonColors.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = myCrayonTableView.dequeueReusableCell(withIdentifier: "crayonCell", for: indexPath)
+        
+        let color = allCrayonColors[indexPath.row]
+        cell.textLabel?.text = color.name
+        cell.detailTextLabel?.text = color.hex
+        
+        let disPlayColor = UIColor.init(displayP3Red: CGFloat(color.red)/255, green: CGFloat(color.green)/255, blue: CGFloat(color.blue)/255, alpha:1)
+        cell.backgroundColor = disPlayColor
+        return cell
+    }
+}
+extension ViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    }
+
+
+
+
+
+
 
--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Views/DetailedViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Views/DetailedViewController.swift
@@ -9,22 +9,81 @@
 import UIKit
 
 class DetailedViewController: UIViewController {
-
+    @IBOutlet weak var colorName: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var redLabel: UILabel!
+    
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var greenLabel: UILabel!
+    
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var stepper: UIStepper!
+    @IBOutlet weak var stepperLabel: UILabel!
+    
+   public var color: Crayon!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+         redSlider.minimumValue = 0.0
+         redSlider.maximumValue = 1.0
+        
 
-        // Do any additional setup after loading the view.
+        blueSlider.minimumValue = 0.0
+        blueSlider.maximumValue = 1.0
+        
+        greenSlider.minimumValue = 0.0
+        greenSlider.maximumValue = 1.0
+        
+        stepper.minimumValue = 0.0
+        stepper.maximumValue = 1.0
+        
+        updateColor()
+    
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func updateColor() {
+        colorName.text = color.name
+        self.view.backgroundColor = UIColor.init(red: CGFloat(redSlider!.value), green: CGFloat(greenSlider!.value), blue: CGFloat(blueSlider!.value), alpha: CGFloat(stepper!.value))
+       
     }
-    */
-
+    
+    @IBAction func redSliderChanged(_ sender: UISlider) {
+        let value = sender.value
+        redLabel.text = "\(value)"
+        updateColor()
+       
+    }
+    
+    @IBAction func greenSliderChanged(_ sender: UISlider) {
+        let value = sender.value
+        greenLabel.text = "\(value)"
+        updateColor()
+       
+    }
+    
+    @IBAction func blueSliderChanged(_ sender: UISlider) {
+        let value = sender.value
+        blueLabel.text = "\(value)"
+        updateColor()
+    }
+    
+    @IBAction func stepperChanged(_ sender: UIStepper) {
+        let value = sender.value
+        stepperLabel.text = "\(value) "
+        updateColor()
+    }
+    
+    @IBAction func reStartButton(_ sender: Any) {
+       
+        greenSlider.value = greenSlider.minimumValue
+        greenLabel.text = "\(greenSlider.minimumValue)"
+        redSlider.value = redSlider.minimumValue
+        redLabel.text = "\(redSlider.minimumValue)"
+        blueSlider.value = blueSlider.minimumValue
+        blueLabel.text = "\(blueSlider.value)"
+    }
 }
