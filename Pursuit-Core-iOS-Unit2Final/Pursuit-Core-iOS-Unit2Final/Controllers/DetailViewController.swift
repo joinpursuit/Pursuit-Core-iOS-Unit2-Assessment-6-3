cf54b99b4477e37333cd29f3e40da9429cec3c4a
maitreebain <maitreebain@gmail.com>
2019-11-22 15:51:42 -0500

    finished assessment!

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/DetailViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/DetailViewController.swift
@@ -31,41 +31,46 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = UIColor(displayP3Red: CGFloat(crayon.red / 255), green: CGFloat(crayon.green / 255), blue: CGFloat(crayon.blue / 255), alpha: 1.0)
+        
+        view.backgroundColor = UIColor(displayP3Red: CGFloat(crayon.red / 255), green: CGFloat(crayon.green / 255), blue: CGFloat(crayon.blue / 255), alpha: 1.0)
         redSliderValues()
         greenSliderValues()
         blueSliderValues()
         alphaSliderValues()
         alphaStepperValues()
+        redTextLabel.textColor = .white
+        greenTextLabel.textColor = .white
+        blueTextLabel.textColor = .white
+        alphaTextLabel.textColor = .white
     }
     
     
     func redSliderValues() {
-        redSlider.minimumValue = 1.0
-        redSlider.maximumValue = 300.0
-        redSlider.value = 50.0
+        redSlider.minimumValue = 0.0
+        redSlider.maximumValue = 50.0
+        redSlider.value = 1.0
     }
 
     func greenSliderValues() {
-        greenSlider.minimumValue = 1.0
-        greenSlider.maximumValue = 300.0
-        greenSlider.value = 50.0
+        greenSlider.minimumValue = 0.0
+        greenSlider.maximumValue = 50.0
+        greenSlider.value = 1.0
     }
     
     func blueSliderValues() {
-        blueSlider.minimumValue = 1.0
-        blueSlider.maximumValue = 300.0
-        blueSlider.value = 50.0
+        blueSlider.minimumValue = 0.0
+        blueSlider.maximumValue = 50.0
+        blueSlider.value = 1.0
     }
     
     func alphaSliderValues() {
-        alphaSlider.minimumValue = 0.1
+        alphaSlider.minimumValue = 0.0
         alphaSlider.maximumValue = 1.0
         alphaSlider.value = 0.1
     }
     
     func alphaStepperValues() {
-        alphaStepper.minimumValue = 0.1
+        alphaStepper.minimumValue = 0.0
         alphaStepper.maximumValue = 1.0
         alphaStepper.value = 0.1
         alphaStepper.stepValue = 0.1
@@ -73,9 +78,9 @@ class DetailViewController: UIViewController {
     
     
     
-    
     @IBAction func redAction(_ sender: UISlider) {
         self.view.backgroundColor = UIColor(displayP3Red: CGFloat(sender.value / 255), green: CGFloat(crayon.green / 255), blue: CGFloat(crayon.blue / 255), alpha: 1.0)
+        redTextLabel.textColor = .white
         redTextLabel.text = "Red: \(round(sender.value))"
 
     }
@@ -83,34 +88,43 @@ class DetailViewController: UIViewController {
     
     @IBAction func greenAction(_ sender: UISlider) {
         self.view.backgroundColor = UIColor(displayP3Red: CGFloat(crayon.red / 255), green: CGFloat(sender.value / 255), blue: CGFloat(crayon.blue / 255), alpha: 1.0)
+        greenTextLabel.textColor = .white
         greenTextLabel.text = "Green: \(round(sender.value))"
     }
     
     @IBAction func blueAction(_ sender: UISlider) {
         self.view.backgroundColor = UIColor(displayP3Red: CGFloat(crayon.red / 255), green: CGFloat(crayon.green / 255), blue: CGFloat(sender.value / 255), alpha: 1.0)
+        blueTextLabel.textColor = .white
         blueTextLabel.text = "Blue: \(round(sender.value))"
     }
     
     @IBAction func alphaAction(_ sender: UISlider) {
         self.view.backgroundColor = UIColor(displayP3Red: CGFloat(crayon.red / 255), green: CGFloat(crayon.green / 255), blue: CGFloat(crayon.blue / 255), alpha: CGFloat(sender.value))
-        alphaTextLabel.text = "Alpha: \(sender.value)"
+        alphaTextLabel.text = String(format: "Alpha: %1.f", sender.value)
         alphaStepper.value = Double(sender.value)
+        alphaTextLabel.textColor = .white
         
     }
     
     
     @IBAction func stepperAction(_ sender: UIStepper) {
         self.view.backgroundColor = UIColor(displayP3Red: CGFloat(crayon.red / 255), green: CGFloat(crayon.green / 255), blue: CGFloat(crayon.blue / 255), alpha: CGFloat(sender.value))
-        alphaTextLabel.text = "Alpha: \(sender.value)"
+        alphaTextLabel.text = String(format: "Alpha: %1.f", sender.value)
         alphaSlider.value = Float(sender.value)
+        alphaTextLabel.textColor = .white
     }
     
     
     @IBAction func resetButton(_ sender: UIButton) {
         redSliderValues()
+        redTextLabel.text = "Red"
         greenSliderValues()
+        greenTextLabel.text = "Green"
         blueSliderValues()
+        blueTextLabel.text = "Blue"
         alphaSliderValues()
         alphaStepperValues()
+        alphaTextLabel.text = "Alpha"
+        self.view.backgroundColor = UIColor(displayP3Red: CGFloat(crayon.red / 255), green: CGFloat(crayon.green / 255), blue: CGFloat(crayon.blue / 255), alpha: 1.0)
     }
 }
