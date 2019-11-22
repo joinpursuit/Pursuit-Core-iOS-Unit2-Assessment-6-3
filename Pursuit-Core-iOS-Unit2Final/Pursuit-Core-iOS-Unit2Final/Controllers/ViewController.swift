397658faf7ab7dfc9542ea6b788ab39cd0174efe
oscar victoria <oscarvictoriagonzalez@gmail.com>
2019-11-22 12:22:09 -0500

    updated colors in table view

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    
+    var separatorColor: UIColor?
+    var sectionIndexColor: UIColor?
+    var sectionIndexBackgroundColor: UIColor?
+    
+    
+    
+    
     var crayon = [Crayon]() {
         didSet {
             tableView.reloadData()
@@ -19,23 +28,23 @@ class ViewController: UIViewController {
     
     
     
-    @IBOutlet weak var tableView: UITableView!
     
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    tableView.dataSource = self
-    loadData()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        loadData()
+        tableView.delegate = self
+        
+    }
     
-  }
-
     func loadData() {
         crayon = Crayon.allTheCrayons
     }
-
-
+    
+    
 }
 
-extension ViewController: UITableViewDataSource {
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return crayon.count
     }
@@ -45,6 +54,28 @@ extension ViewController: UITableViewDataSource {
         let someCrayon = crayon[indexPath.row]
         cell.textLabel?.text = someCrayon.name
         cell.detailTextLabel?.text = someCrayon.hex
+        cell.backgroundView?.backgroundColor = UIColor(red: CGFloat(someCrayon.red), green:CGFloat(someCrayon.green), blue: CGFloat(someCrayon.blue), alpha: CGFloat(someCrayon.blue))
+        let backgroundView = UIView()
+        backgroundView.backgroundColor = UIColor.clear
+        cell.selectedBackgroundView = backgroundView
+        
+        
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        let someColors = crayon[indexPath.row]
+        let theColors = UIColor(red: CGFloat(someColors.red)/CGFloat(255),
+                                green: CGFloat(someColors.green)/CGFloat(255),
+                                blue: CGFloat(someColors.blue)/CGFloat(255),
+        alpha: 1)
+        cell.backgroundColor = theColors
+    }
+    
+    
+    
 }
+
+//for color in crayon {
+//    cell.backgroundColor = UIColor()
+//}
