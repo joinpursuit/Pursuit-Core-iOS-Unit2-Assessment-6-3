e462f69a330fb4e4c25c09bec43256d513f82dba
Ameni Alsaydi <amenialsaydi@pursuit.org>
2019-11-22 14:52:12 -0500

    labels change to a lighter color once background gets too dark

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Views/DetailViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Views/DetailViewController.swift
@@ -20,21 +20,33 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var alphaStepper: UIStepper!
     @IBOutlet weak var alphaLabel: UILabel!
     
+    @IBOutlet var allLabels: [UILabel]!
+    
     
     var crayon: Crayon?
+    var labelAlpha = 1.0
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         colorNameLabel.text = crayon?.name
         
-        configureControls() // configuring only needs to happen once.
-        updateBackgroundColor()
+        configureControls()
+        updateBackgroundandLabelColors()
 
     }
     
-    func updateBackgroundColor() {
+    func updateBackgroundandLabelColors() {
         
         view.backgroundColor = UIColor(displayP3Red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(alphaStepper.value))
+        
+        if redSlider.value < 0.5 && greenSlider.value < 0.5 && blueSlider.value < 0.5 || alphaStepper.value < 0.5 {
+            allLabels.map {$0.textColor = .white}
+        } else {
+            allLabels.map {$0.textColor = .black}
+        }
+        
     }
     
     func configureControls() {
@@ -45,57 +57,60 @@ class DetailViewController: UIViewController {
         redSlider.maximumValue = 1.0
         redSlider.value = Float(selectedCrayon.red/255)
         redLabel.text = String(format: "%.2f", redSlider.value)
+        //redLabel.alpha = CGFloat(alphaStepper.value)
         
         // green
         greenSlider.minimumValue = 0.0
         greenSlider.maximumValue = 1.0
         greenSlider.value = Float(selectedCrayon.green/255)
         greenLabel.text = String(format: "%.2f", greenSlider.value)
+        //greenLabel.alpha = CGFloat(alphaStepper.value)
         
         //blue
         blueSlider.minimumValue = 0.0
         blueSlider.maximumValue = 1.0
         blueSlider.value = Float(selectedCrayon.blue/255)
         blueLabel.text = String(format: "%.2f", blueSlider.value)
+        //blueLabel.alpha = CGFloat(alphaStepper.value)
         
         //alpha
         alphaStepper.minimumValue = 0.0
         alphaStepper.maximumValue = 1.0
         alphaStepper.stepValue = 0.1
         alphaStepper.value = 1.0
-        alphaLabel.text = String(format: "%.1f", alphaStepper.value)
+        alphaLabel.text = "Alpha: \(String(format: "%.1f", alphaStepper.value))"
+        //alphaLabel.alpha = CGFloat(alphaStepper.value)
         
         
-         
     }
-    
     
     // Actions
     
     @IBAction func redSliderChanged(_ sender: UISlider) {
         redLabel.text = String(format: "%.2f", sender.value)
-        updateBackgroundColor()
+        updateBackgroundandLabelColors()
         
     }
     
     @IBAction func greenSliderChanged(_ sender: UISlider) {
         greenLabel.text = String(format: "%.2f", sender.value)
-        updateBackgroundColor()
+        updateBackgroundandLabelColors()
     }
     
     @IBAction func blueSliderChanged(_ sender: UISlider) {
         blueLabel.text = String(format: "%.2f", sender.value)
-               updateBackgroundColor()
+               updateBackgroundandLabelColors()
     }
     
     @IBAction func alphaStepperChanged(_ sender: UIStepper) {
-        alphaLabel.text = String(format: "%.1f", sender.value)
-        updateBackgroundColor()
+        alphaLabel.text = "Alpha: \(String(format: "%.1f", sender.value))"
+        updateBackgroundandLabelColors()
     }
     
     @IBAction func reset(_ sender: UIButton) {
         configureControls()
-        updateBackgroundColor()
+        updateBackgroundandLabelColors()
+        allLabels.map {$0.textColor = .black}
         
     }
     
