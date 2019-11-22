8fb883863a1b0dfd1b655077839bd265acaccfc7
Yuliia Engman <yuliia.engman@gmail.com>
2019-11-22 10:51:44 -0500

    set tableView with name and suntitle withput color yet

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
@@ -9,12 +9,78 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var crayonColors = [Crayon] () {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        loadData()
+    }
+    
+    func loadData() {
+        crayonColors = Crayon.allTheCrayons
+    }
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return crayonColors.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "crayonCell", for: indexPath)
+        let color = crayonColors[indexPath.row]
+        cell.textLabel?.text = color.name
+        cell.detailTextLabel?.text = color.hex
+        return cell
+    }
+}
+
+
+//    var animals = [ZooAnimal]() {
+//        didSet { //property observer - want automatically reload tableview
+//            tableView.reloadData()
+//        }
+//    }
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        tableView.dataSource = self
+//        loadData()
+//    }
+//
+//    func loadData() {
+//        animals = ZooAnimal.zooAnimals
+//    }
+//
+//
+//}
+//
+//extension BasicAnimalCellController: UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return animals.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+////        this is for custom cells
+////        let cell = tableView.dequeueReusableCell(withIdentifier: "animalCell", for: indexPath)
+//
+//        //this is using a default cell
+//
+//        let cell = UITableViewCell()
+//
+//        // get current animal
+//        let animal = animals[indexPath.row]
+//
+//        // configure cell to be a "subtitle"
+//        cell.imageView?.image = UIImage(named: animal.imageNumber.description) // gives us String instead of Intr
+//        cell.textLabel?.text = animal.name
+//        cell.detailTextLabel?.text = animal.origin
+//        return cell
+//    }
