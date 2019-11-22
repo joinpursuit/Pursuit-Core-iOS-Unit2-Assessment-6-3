7da3185bb3affe155188c5019440e9e5d9f1b3db
maitreebain <maitreebain@gmail.com>
2019-11-22 10:28:48 -0500

    Ui is set up working on view controllers

---
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/DetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.swift
+//  Pursuit-Core-iOS-Unit2Final
+//
+//  Created by Maitree Bain on 11/22/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
