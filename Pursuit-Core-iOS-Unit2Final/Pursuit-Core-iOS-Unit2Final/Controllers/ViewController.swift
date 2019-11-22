e233a404d7f10007d719e96e7f61daf65039ce9c
oscar victoria <oscarvictoriagonzalez@gmail.com>
2019-11-22 10:38:50 -0500

    updated view controller and connected table view to reflect a table view with crayon data

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var crayon = [Crayon]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    tableView.dataSource = self
+    loadData()
+    
   }
+
+    func loadData() {
+        crayon = Crayon.allTheCrayons
+    }
 
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return crayon.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "crayonCell", for: indexPath)
+        let someCrayon = crayon[indexPath.row]
+        cell.textLabel?.text = someCrayon.name
+        cell.detailTextLabel?.text = someCrayon.hex
+        return cell
+    }
+}
